7c7c3a8e1ebaf7f8f0ae5a0bd9c100be52fab60f
afelices <antoniojfelicescruz@gmail.com>
2024-11-30 21:51:58 +0100

    Styles applied from XAML

--- POC_4friends_mobile/SharedResources.cs
+++ POC_4friends_mobile/SharedResources.cs
@@ -3,8 +3,9 @@ namespace POC_4friends_mobile
 {
     static class SharedResources
     {
-        public static readonly Color FontColorWhite = Color.FromRgb(0xFF, 0xFF, 0xFF);
-        public static readonly Color FontColorBlue = Color.FromRgb(0, 0, 0xCC);
-        public static readonly Color BackgroundColor = Color.FromRgb(0, 0, 0xCC);
+        public static readonly Color LightFontColor = Color.FromRgb(0xFF, 0xFF, 0xFF);          //white
+        public static readonly Color DarkFontColor = Color.FromRgb(0xF6, 0x99, 0xCD);           //pink
+        public static readonly Color LightBackgroundColor = Color.FromRgb(0xF6, 0x99, 0xCD);    //pink
+        public static readonly Color DarkBackgroundColor = Color.FromRgb(0, 0, 0);              //black
     }
 }
