61138c03072b46e6e7e0e462c864fb6fc6cc2dc9
afelices <antoniojfelicescruz@gmail.com>
2024-11-30 20:09:32 +0100

    User model added

---
+++ POC_4friends_mobile/Models/User.cs
@@ -0,0 +1,13 @@
+﻿
+using SQLite;
+
+namespace POC_4friends_mobile.Models
+{
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
